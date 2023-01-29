dfc906464e47b76c7c8470a77dc87ef566a9d445
cr0w- <cr0w-@MILEN-PC>
2023-01-29 12:16:30 +0200

    sync

---
+++ 01. Train/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse(Console.ReadLine());
+int totalSum = 0;
+
+int[] myArr = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    myArr[i] = int.Parse(Console.ReadLine());
+    totalSum += myArr[i];
+    
+}
+
+for (int i = 0; i < myArr.Length; i++)
+    Console.Write($"{myArr[i]} ");
+
+Console.WriteLine();
+Console.WriteLine( totalSum );  
+
---
+++ 02. Common Elements/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+
+string firstString = Console.ReadLine();
+string secondString = Console.ReadLine();
+
+string[] firstStringArray = firstString.Split(' ');
+string[] secondStringArray = secondString.Split(' ');
+
+for (int i = 0; i < secondStringArray.Length; i++)
+{
+    for (int j = 0; j < firstStringArray.Length; j++)
+    {
+        if (secondStringArray[i] == firstStringArray[j])
+        {
+            Console.Write($"{secondStringArray[i]} ");
+        }
+    }
+}
