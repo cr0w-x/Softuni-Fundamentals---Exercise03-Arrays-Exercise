82d9675712b2cb937fe301a89a9a421338aee5e6
cr0w- <cr0w-@MILEN-PC>
2023-01-29 20:00:19 +0200

    sync

---
+++ 05. Top Integers/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();
+for (int i = 0; i < values.Length; i++)
+{
+    bool isTopInteger = true;
+    for (int k = i + 1; k < values.Length; k++)
+    {
+        if (values[i] <= values[k])
+        {
+            isTopInteger = false;
+        }
+    }
+
+    if (isTopInteger)
+    {
+        Console.Write(values[i] + " ");
+    }
+}
