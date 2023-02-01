f7b01b8d71f38e2d4c1e930890ffb5d2827b9ef1
cr0w- <cr0w-@MILEN-PC>
2023-02-01 17:19:55 +0200

    sync

---
+++ 06. Equal Sum/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int sumLeft = 0;
+int sumRight = 0;
+bool flag = false;
+
+for (int i = 0;i < values.Length; i++)
+{
+    sumLeft = 0;
+    sumRight = 0;
+
+    for (int j = i - 1; j >= 0; j--)
+    {
+        sumLeft += values[j];
+    }        
+
+    for (int k = i+1; k < values.Length; k++)
+    {
+        sumRight += values[k];
+    }
+
+    if (sumLeft == sumRight)
+    {
+        Console.WriteLine(i);
+        flag = true;
+    }  
+}
+
+if (flag == false && values.Length >= 2)
+{
+    Console.WriteLine("no");
+}
+
+else if (flag == false && values.Length < 2)
+{
+    Console.WriteLine("0");   
+}
