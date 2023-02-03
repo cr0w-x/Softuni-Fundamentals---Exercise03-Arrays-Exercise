a8c18246b9081424120e2da8a62e94c2d71bd77a
cr0w- <cr0w-@MILEN-PC>
2023-02-03 10:46:11 +0200

    sync

---
+++ 07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Metrics;
+
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int counter = 0;
+int biggest = 0;
+int index = 0;
+
+
+for (int i = 0;i < numbers.Length-1; i++)
+{    
+    if (numbers[i] == numbers[i + 1])
+    {
+        counter++;        
+    }
+
+    else
+    {
+        counter = 0;
+    }
+
+    if (biggest < counter )
+    {
+        biggest = counter;
+        index = i;
+    }
+}
+
+int output = numbers[index];
+
+for (int i = 0; i <= biggest; i++)
+    Console.Write($"{output} ");
