201c811ca83fc9eaaf33f3a8f05298404f3adf73
cr0w- <cr0w-@MILEN-PC>
2023-02-04 16:34:44 +0200

    sync

---
+++ 08. Magic Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] givenNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int magicNumber = int.Parse(Console.ReadLine());
+
+for (int i = 0;i < givenNumbers.Length; i++)
+{
+    for (int j = i +1; j < givenNumbers.Length; j++)
+    {
+        if (givenNumbers[i] + givenNumbers[j]==magicNumber)
+        {
+            Console.Write($"{givenNumbers[i]} {givenNumbers[j]}");
+            Console.WriteLine();
+        }        
+    }   
+}
